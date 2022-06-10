084a2a75f50e798cbd3b48c5c691f4d65fca3e42
xmk <xingmk78@gmail.com>
2022-06-10 19:36:15 +0800

    100题

--- School/Practices/10-19/19.c
+++ School/Practices/10-19/19.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  static int k[10];
+  int i, j, n, s;
+  for (j = 2; j < 1000; j++) {
+    n = -1;
+    s = j;
+    for (i = 0; i < ; i++) {
+    }
+  }
+
+  return 0;
+}
