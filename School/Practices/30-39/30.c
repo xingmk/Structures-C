80487965c80131a0367a8223db9751776b54cf49
xmk <xingmk78@gmail.com>
2022-06-16 17:03:51 +0800

    C topic

--- School/Practices/30-39/30.c
+++ School/Practices/30-39/30.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+
+  int x, ge, shi, bai, qian, wan;
+  ge = x % 10;
+  shi = x % 100 / 10;
+  bai = x % 1000 / 100;
+  qian = x / 1000 % 10;
+  wan = x / 10000;
+
+  if (ge == wan && shi == qian) {
+    printf("%d is huiwen", x);
+  } else {
+    printf("%dthis isn't huiwen", x);
+  }
+  return 0;
+}
