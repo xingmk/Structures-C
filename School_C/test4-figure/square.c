8b4903292570641176ff46c5320e4c311fc233ff
xmk <xingmk78@gmail.com>
2022-06-23 12:19:16 +0800

    C

--- School_C/test4-figure/square.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-  const double r = 5;
-  printf("square radii:%lf\n", r);
-  printf("the area of the square is:%lf\n", 3.1415926 * r * r);
-  printf("the girth of the square is:%lf\n", 2 * 3.1415926 * r);
-
-  return 0;
-}
