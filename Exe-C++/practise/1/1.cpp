12c3f3aefd280bfe4ac51543e28ee63717434afc
Xingmk <xingmk78@gmail.com>
2022-07-07 18:20:48 +0800

    递归分鱼

--- Exe-C++/practise/1/1.cpp
+++ Exe-C++/practise/1/1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int fish(int n, int x)
+int fish(int n, int x) //判断捕鱼数
 {
     if ((x - 1) % 5 == 0) {
         if (n == 1) {
-            return 1;/*递归出口*/
+            return 1; /*递归出口*/
         } else
-            return fish(n - 1, (x - 1) / 5 * 4);/*递归调用*/
+            return fish(n - 1, (x - 1) / 5 * 4); /*递归调用*/
     }
-    return 0;/*x不是符合题意的解，返回0*/
+    return 0; /*x不是符合题意的解，返回0*/
 }
 
 int main(int argc, char* argv[])
@@ -19,11 +19,11 @@ int main(int argc, char* argv[])
     do {
         i = i + 1;
         x = i * 5 + 1; /*x最小值为6，以后每次增加5*/
-        if (fish(5, x)) { /*x最小值为6，以后每次增加5*/
+        if (fish(5, x)) {
             flag = 1;
             cout << "捕鱼总数" << x << endl;
         }
-    } while (!flag);/*未找到符合题意的x，继续循环，否则退出循环*/
+    } while (!flag); /*未找到符合题意的x，继续循环，否则退出循环*/
 
     return 0;
 }
