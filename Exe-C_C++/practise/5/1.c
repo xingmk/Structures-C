203b90b6d8ba62ca0d56a8f09ccec0fd77de1e62
Xingmk <xingmk78@gmail.com>
2022-07-08 19:10:00 +0800

    删除星号

---
+++ Exe-C_C++/practise/5/1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fun(char *a, char *h, char *p) {
+  int i, j;
+  // 删除指针 h p之间所有*
+  for (i = 0, j = 0; &h[i] < p; i++) {
+    if (h[i] != '*') {
+      h[j++] = h[i];
+    }
+    // 将指针p至字符串尾部的所有字符前移
+    // 补齐前面所删除字符中的*
+    for (i = 0; p[i]; i++, j++)
+      h[j] = p[i];
+  }
+  h[j] = '\0';
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  char s[81], *t, *f;
+  printf("Enter a string :\n");
+  scanf("%s", s); /*输入字符串*/
+  t = f = s;      /*用字符指针t、f指向串s*/
+
+  while (*t)
+    t++;
+  t--;              /*将指针t定位到字符串中最后一个字符*/
+  while (*t == '*') /*指针t指向字符串中最后一个字符*/
+    t--;
+  while (*f == '*') /*指针f指向字符串中第一个字符*/
+    f++;
+  fun(s, f, t);
+  printf("The string after deleted:\n"); /*输出结果*/
+  puts(s);
+
+  return 0;
+}
